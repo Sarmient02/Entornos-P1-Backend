b1831819a693570b205e56593d54c858b7fca613
Javier Sarmiento <javiersarmiento02@gmail.com>
2023-10-18 17:16:35 -0500

    intialize spring project

---
+++ src/main/java/com/entornos/p1/backuistudy/BackUistudyApplication.java
@@ -0,0 +1,13 @@
+package com.entornos.p1.backuistudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackUistudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackUistudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/entornos/p1/backuistudy/BackUistudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entornos.p1.backuistudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackUistudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
