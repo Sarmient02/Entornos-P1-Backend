519df301390506e437655c311ed667ebb8fe86bd
Javier Sarmiento <javiersarmiento02@gmail.com>
2023-10-20 17:24:09 -0500

    feat(cors): adding configuration

--- src/main/java/com/entornos/p1/backuistudy/controller/PruebaController.java
+++ src/main/java/com/entornos/p1/backuistudy/controller/PruebaController.java
@@ -3,7 +3,6 @@ package com.entornos.p1.backuistudy.controller;
 import com.entornos.p1.backuistudy.model.TestUsers;
 import com.entornos.p1.backuistudy.service.impl.PruebaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/prueba")
-@CrossOrigin(origins = {"https://entornos-p1-frontend.onrender.com", "http://127.0.0.1:5501/"})
 public class PruebaController {
     @Autowired
     private PruebaServiceImpl testUserService;
